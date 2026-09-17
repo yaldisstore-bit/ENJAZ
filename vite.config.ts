7651ca09544eda44d3f92f404625a63350f70c51
yaldisstore-bit <yaldis.store@gmail.com>
2026-09-17 17:49:43 +0300

    phase11.5-D: tighten production bundle emission

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
       optimization: { inlineConst: true },
       output: {
         minify: true,
+        topLevelVar: true,
         codeSplitting: {
           groups: [
             { name: 'react-vendor', test: /node_modules[\\/](react|react-dom|react-router|scheduler)([\\/]|$)/, priority: 30 },
